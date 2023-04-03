51f361af117c76a27301625e1081ee243f92826b
Yotsu <tatsuyoshi.nyan.0125@gmai.com>
2023-04-03 22:04:59 +0900

    [Add] Init

---
+++ MVVMSample/MVVMSample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MVVMSample
+//
+//  Created by 樹佳 on 2023/04/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVMSample/MVVMSample/MVVMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSampleApp.swift
+//  MVVMSample
+//
+//  Created by 樹佳 on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
